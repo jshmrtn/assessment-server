7ce36de56b69e211f70a81a2fb426133e5c03cc9
Jonatan Männchen <jonatan@maennchen.ch>
2017-08-25 09:34:49 +0000

    Build & Config

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,5 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# Configures the endpoint
 config :assessment_server, AssessmentServerWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "p2DzUxkEeGA/a1Q0VBVvI7p5d0TEDOysY5wUCHqMtzQ70amuhsDvGULPhxCD9IUT",
@@ -13,11 +7,8 @@ config :assessment_server, AssessmentServerWeb.Endpoint,
   pubsub: [name: AssessmentServer.PubSub,
            adapter: Phoenix.PubSub.PG2]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
